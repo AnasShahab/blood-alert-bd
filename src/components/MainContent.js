5c6f4284be0709c6b762baefd5aef9b81f87dccc
AnasShahab <anas.shahab75@gmail.com>
2020-12-08 18:21:48 +0100

    Testing

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,11 @@
 import React from 'react'
-//import logo from '.images//Logo-Draft.png'
+import logo from './images/Logo-Draft.png'
 
 function MainContent(){
     return(
         <div>
             <h1>Blood Alerting BD</h1>
-            <img src="http://placekitten.com/300/200" alt="icon" />
+            <img src={logo} alt="logo" />
         </div>
     )
 }
