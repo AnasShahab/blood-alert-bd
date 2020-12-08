a76872d1a85e74d8d967db53ca99152e5584c505
AnasShahab <anas.shahab75@gmail.com>
2020-12-08 15:35:59 +0100

    Updated

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Logo from "~/blood-alert-bd/blood-alert-bd/src/images/Logo-Draft.png"
 
 function MainContent(){
     return(
         <div>
+            <img src={Logo}></img>
             <h1>Blood Alert BD</h1>
         </div>
     )
