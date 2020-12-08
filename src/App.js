b4bcf879e45f3ee3653003f0a60e45f5b570a1f3
AnasShahab <anas.shahab75@gmail.com>
2020-12-08 15:17:01 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 //import logo from './logo.svg';
 //import './App.css';
+import React from 'react'
+import MainContent from "./components/MainContent.js"
 
-
+function App(){
+  return(
+    <MainContent />
+  )
+}
 
 export default App;
